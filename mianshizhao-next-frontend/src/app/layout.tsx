90d307c0ad54a4772766ebfd60e91e372635204c
liangjunzhao <247990824@qq.com>
2024-11-23 02:30:07 +0800

    完成前置初始化

--- mianshizhao-next-frontend/src/app/layout.tsx
+++ mianshizhao-next-frontend/src/app/layout.tsx
@@ -1,20 +1,48 @@
-import { AntdRegistry } from "@ant-design/nextjs-registry";
+"use client"
+import {AntdRegistry} from "@ant-design/nextjs-registry";
 import "./globals.css";
 import BasicLayout from "@/layout/BasicLayout";
-import React from "react";
+import React, {useCallback, useEffect} from "react";
+
+/**
+ * 全局初始化逻辑
+ * @param children
+ * @constructor
+ */
+const InitLayout: React.FC<Readonly<{
+    children: React.ReactNode;
+}>> = ({children}) => {
+
+    // useCallback用来缓存函数，当[]中的参数发生变化的时候才会再次执行;
+    const doInit = useCallback(() => {
+        console.log("init");
+    }, []);
+
+    // 这是个钩子函数，当[]中的参数发生变化的时候才会执行这个函数，如果不传东西就执行一次
+    useEffect(() => {
+        doInit();
+    }, []);
+
+    return children;
+}
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="zh">
-      <body>
+    return (
+        <html lang="zh">
+        <body>
+
         <AntdRegistry>
-          <BasicLayout>{children}</BasicLayout>
+            <InitLayout>
+                <BasicLayout>{children}</BasicLayout>
+            </InitLayout>
         </AntdRegistry>
-      </body>
-    </html>
-  );
+
+
+        </body>
+        </html>
+    );
 }
