83348e3bcfdfed8ac276491ca3dd548b33260329
liangjunzhao <247990824@qq.com>
2024-11-22 22:44:35 +0800

    change1

--- mianshizhao-backend/src/main/java/com/zhao/mianshizhao/service/QuestionBankQuestionService.java
+++ mianshizhao-backend/src/main/java/com/zhao/mianshizhao/service/QuestionBankQuestionService.java
@@ -12,8 +12,6 @@ import javax.servlet.http.HttpServletRequest;
 /**
  * 题目题库关联服务
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://www.code-nav.cn">编程导航学习圈</a>
  */
 public interface QuestionBankQuestionService extends IService<QuestionBankQuestion> {
 
